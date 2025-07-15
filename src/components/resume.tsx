6271d0c0e5cab819c33daca7b9ae3aa55079b554
ItzNiusha <166985686+ItzNiusha@users.noreply.github.com>
2025-07-15 21:58:17 +0000

    upd resume

--- src/components/resume.tsx
+++ src/components/resume.tsx
@@ -14,7 +14,7 @@ export function Resume() {
     lastUpdated: 'Jul 2025',
     fileSize: '0.2 MB',
     previewImageSrc: '',
-    downloadUrl: '/NiushasResume.pdf',
+    downloadUrl: '/NessasResume.pdf',
   };
 
   const handleDownload = () => {
